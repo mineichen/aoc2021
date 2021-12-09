3400dce44b7432aac87727eadbcafeea8b608675
Markus Ineichen <kusi.ineichen@gmail.com>
2021-12-09 21:42:00 +0100

    Solve with pathfinding

--- src/day9.rs
+++ src/day9.rs
@@ -1,145 +1,58 @@
-use std::{collections::HashMap, io::Read};
+use itertools::{iproduct, Itertools};
+use pathfinding::prelude::{bfs_reach, Matrix};
 
-use itertools::{process_results, Itertools};
-use simple_lines::ReadExt;
-
-fn parse(r: impl Read) -> Result<Puzzle, Box<dyn std::error::Error>> {
-    let mut data = Vec::new();
-    let mut iter = r.lines_rc();
-    data.extend(
-        iter.next()
-            .ok_or(crate::utils::AocError::EmptyInput)??
-            .chars()
-            .map(|c| (c as u8) - '0' as u8),
-    );
-    let mut width = data.len();
-    for line in iter {
-        data.extend(line?.chars().map(|c| (c as u8) - '0' as u8));
-    }
-    let height = data.len() / width;
-    Ok(Puzzle {
-        width,
-        data,
-        height,
-    })
+fn lows(m: &Matrix<u8>) -> impl Iterator<Item = (usize, usize)> + '_ {
+    iproduct!(0..m.rows, 0..m.columns).filter(|&k| m.neighbours(&k, false).all(|n| m[&n] > m[&k]))
 }
 
-impl Puzzle {
-    fn find_low_points(&self) -> Vec<usize> {
-        let Puzzle {
-            data,
-            width,
-            height,
-        } = self;
-
-        let mut min_indices = Vec::new();
-        let mut xy = 0;
-        for y in 0..*height {
-            for x in 0..*width {
-                let cur = data[xy];
-                let top_min = if y == 0 {
-                    false
-                } else {
-                    data[xy - width] <= cur
-                };
-                let left_min = if x == 0 { false } else { data[xy - 1] <= cur };
-                let bottom_min = if y == height - 1 {
-                    false
-                } else {
-                    data[xy + width] <= cur
-                };
-                let right_min = if x == width - 1 {
-                    false
-                } else {
-                    data[xy + 1] <= cur
-                };
-
-                if !(top_min || left_min || bottom_min || right_min) {
-                    min_indices.push(xy);
-                }
-                xy += 1;
-            }
-        }
-
-        min_indices
-    }
-
-    fn low_point_risk_level(&self) -> i32 {
-        self.find_low_points()
-            .into_iter()
-            .map(|idx| self.data[idx] as i32 + 1)
-            .sum::<i32>()
-    }
-
-    fn flood_fill(&mut self, i: usize) -> i32 {
-        if self.data[i] == 9 {
-            return 0;
-        }
-        self.data[i] = 9;
-        let x = i % self.width;
-        let y = i / self.width;
-        let mut count = 1;
-        if x > 0 {
-            count += self.flood_fill(i - 1)
-        }
-        if y > 0 {
-            count += self.flood_fill(i - self.width);
-        }
-        if x < self.width - 1 {
-            count += self.flood_fill(i + 1)
-        }
-        if y < self.height - 1 {
-            count += self.flood_fill(i + self.width);
-        }
-        count
-    }
-    fn top_lake_sizes(mut self, n: usize) -> i32 {
-        self.find_low_points()
-            .iter()
-            .map(|i| self.flood_fill(*i))
-            .sorted_by(|a, b| b.cmp(&a))
-            .take(n)
-            .product::<i32>()
-    }
+fn calculate_risk_level(input: &str) -> u32 {    
+    let m = input.lines().map(|c| c.bytes()).collect();        
+    lows(&m).map(|k| (m[&k] - b'0') as u32 + 1).sum::<u32>()
 }
 
-struct Puzzle {
-    width: usize,
-    height: usize,
-    data: Vec<u8>,
+fn count_top_lake_tiles(input: &str, n: usize) -> usize {
+    let m = input.lines().map(|c| c.bytes()).collect();
+    lows(&m)
+        .map(|n| {
+            bfs_reach(n, |&n| {
+                m.neighbours(&n, false)
+                    .filter(|&k| m[&k] != b'9' && m[&k] > m[&n])
+                    .collect_vec()
+            })
+            .count()
+        })
+        .sorted_unstable_by(|a, b| b.cmp(a))
+        .take(n)
+        .product()
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use std::io::Read;
 
     #[test]
     fn part1() {
-        let puzzle = parse(std::fs::File::open("puzzleData/day9.txt").unwrap()).unwrap();
-        assert_eq!(458, puzzle.low_point_risk_level())
+        let mut buf = String::new();
+        std::fs::File::read_to_string(&mut std::fs::File::open("puzzleData/day9.txt").unwrap(), &mut buf).unwrap();
+        assert_eq!(458, calculate_risk_level(&buf));
     }
     #[test]
     fn part1_test() {
-        let puzzle = parse(std::io::Cursor::new(TESTDATA)).unwrap();
-        assert_eq!(15, puzzle.low_point_risk_level())
-    }
-
-    #[test]
-    fn part1_lowpoints() {
-        let puzzle = parse(std::io::Cursor::new(TESTDATA)).unwrap();
-        assert_eq!(vec!(1, 9, 22, 46), puzzle.find_low_points())
+        assert_eq!(15, calculate_risk_level(TESTDATA));
     }
 
     #[test]
     fn part2_test() {
-        let puzzle = parse(std::io::Cursor::new(TESTDATA)).unwrap();
-        assert_eq!(1134, puzzle.top_lake_sizes(3));
+        let mut buf = String::new();
+        std::fs::File::read_to_string(&mut std::fs::File::open("puzzleData/day9.txt").unwrap(), &mut buf).unwrap();
+        assert_eq!(1391940, count_top_lake_tiles(&buf, 3));
     }
 
     #[test]
     fn part2() {
-        let puzzle = parse(std::fs::File::open("puzzleData/day9.txt").unwrap()).unwrap();
-        assert_eq!(1391940, puzzle.top_lake_sizes(3));
+        assert_eq!(1134, count_top_lake_tiles(TESTDATA, 3));
     }
 
     const TESTDATA: &'static str = "2199943210
