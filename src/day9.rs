89b363b87ddf64b78734046711c533c92d127aa7
Markus Ineichen <kusi.ineichen@gmail.com>
2021-12-09 22:09:44 +0100

    Simplify

--- src/day9.rs
+++ src/day9.rs
@@ -2,7 +2,7 @@ use itertools::{iproduct, Itertools};
 use pathfinding::prelude::{bfs_reach, Matrix};
 
 fn lows(m: &Matrix<u8>) -> impl Iterator<Item = (usize, usize)> + '_ {
-    iproduct!(0..m.rows, 0..m.columns).filter(|&k| m.neighbours(&k, false).all(|n| m[&n] > m[&k]))
+    iproduct!(0..m.rows, 0..m.columns).filter(|k| m.neighbours(k, false).all(|n| m[&n] > m[k]))
 }
 
 fn calculate_risk_level(input: &str) -> u32 {    
@@ -14,9 +14,9 @@ fn count_top_lake_tiles(input: &str, n: usize) -> usize {
     let m = input.lines().map(|c| c.bytes()).collect();
     lows(&m)
         .map(|n| {
-            bfs_reach(n, |&n| {
-                m.neighbours(&n, false)
-                    .filter(|&k| m[&k] != b'9' && m[&k] > m[&n])
+            bfs_reach(n, |n| {
+                m.neighbours(n, false)
+                    .filter(|k| m[k] != b'9')
                     .collect_vec()
             })
             .count()
