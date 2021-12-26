31b5655d1610fe0285860713444f3a8c2beb853a
Markus Ineichen <kusi.ineichen@gmail.com>
2021-12-26 18:53:39 +0100

    Reformat

--- src/day14.rs
+++ src/day14.rs
@@ -1,6 +1,6 @@
-use std::{io::Read, collections::{HashMap}};
 use itertools::{Itertools, MinMaxResult};
 use simple_lines::ReadExt;
+use std::{collections::HashMap, io::Read};
 
 use crate::{utils::AocError, DynError};
 fn parse(input: impl Read) -> Result<(Vec<ListItem>, Vec<((u8, u8), u8)>, u8), DynError> {
@@ -14,37 +14,46 @@ fn parse(input: impl Read) -> Result<(Vec<ListItem>, Vec<((u8, u8), u8)>, u8), D
         .map(|(index, value)| {
             let len = known.len();
             let value = *known.entry(value).or_insert(len as u8);
-            ListItem{ value, next: index + 1 }
+            ListItem {
+                value,
+                next: index + 1,
+            }
         })
         .collect();
 
-    let rules = lines.skip(1).map(|line| { 
-        let line = line?;
-        let mut chars = line.bytes();
-        let before = chars.next().ok_or(AocError::InvalidRowFormat)?;
-        let after = chars.next().ok_or(AocError::InvalidRowFormat)?;
-        let insert = chars.nth(4).ok_or(AocError::InvalidRowFormat)?;
+    let rules = lines
+        .skip(1)
+        .map(|line| {
+            let line = line?;
+            let mut chars = line.bytes();
+            let before = chars.next().ok_or(AocError::InvalidRowFormat)?;
+            let after = chars.next().ok_or(AocError::InvalidRowFormat)?;
+            let insert = chars.nth(4).ok_or(AocError::InvalidRowFormat)?;
 
-        let len = known.len() as u8;
-        let before = *known.entry(before).or_insert(len);
-        let len = known.len() as u8;
-        let after = *known.entry(after).or_insert(len);        
-        let len = known.len() as u8;
-        let insert = *known.entry(insert).or_insert(len);
+            let len = known.len() as u8;
+            let before = *known.entry(before).or_insert(len);
+            let len = known.len() as u8;
+            let after = *known.entry(after).or_insert(len);
+            let len = known.len() as u8;
+            let insert = *known.entry(insert).or_insert(len);
 
-        Ok(((before, after), insert))    
-    }).collect::<Result<Vec<_>, DynError>>()?;
+            Ok(((before, after), insert))
+        })
+        .collect::<Result<Vec<_>, DynError>>()?;
     Ok((initial, rules, known.len() as u8))
 }
-// : 
+// :
 #[derive(Clone, Copy, Debug)]
 struct ListItem {
     value: u8,
-    next: usize
+    next: usize,
 }
 
-fn try_apply_rules(values: &mut Vec<ListItem>, rules: impl Iterator<Item=Result<((u8, u8), u8), DynError>>) -> Result<(), DynError>  {
-    let rules_map : Result<HashMap<_, _>, Box::<dyn std::error::Error>> = rules.collect();
+fn try_apply_rules(
+    values: &mut Vec<ListItem>,
+    rules: impl Iterator<Item = Result<((u8, u8), u8), DynError>>,
+) -> Result<(), DynError> {
+    let rules_map: Result<HashMap<_, _>, Box<dyn std::error::Error>> = rules.collect();
     apply_rules(values, &rules_map?);
     Ok(())
 }
@@ -54,17 +63,25 @@ fn apply_rules(value: &mut Vec<ListItem>, rules: &HashMap<(u8, u8), u8>) {
     for _ in 0..value.len() - 1 {
         let next = value[current.1.next];
         if let Some(insert) = rules.get(&(current.1.value, next.value)) {
-            value.push(ListItem{ value: *insert, next: current.1.next });
+            value.push(ListItem {
+                value: *insert,
+                next: current.1.next,
+            });
             value[current.0].next = value.len() - 1;
         }
-        current = (current.1.next, next);  
+        current = (current.1.next, next);
     }
 }
 
-fn apply_n_rules_fast(value: Vec<ListItem>, rules: Vec<((u8, u8), u8)>, n: usize, unique: u8) -> MinMaxResult<u64> {
+fn apply_n_rules_fast(
+    value: Vec<ListItem>,
+    rules: Vec<((u8, u8), u8)>,
+    n: usize,
+    unique: u8,
+) -> MinMaxResult<u64> {
     let unique = unique as usize;
     let unique_square = unique * unique;
-    let mut actual = vec!(0u64; unique_square);
+    let mut actual = vec![0u64; unique_square];
     let last = match value.last() {
         Some(x) => x.value as usize,
         None => return MinMaxResult::NoElements,
@@ -78,12 +95,12 @@ fn apply_n_rules_fast(value: Vec<ListItem>, rules: Vec<((u8, u8), u8)>, n: usize
         actual[(before as usize * unique + after as usize)] += 1;
     }
 
-    let mut rules_matrix = vec!(u8::MAX; unique_square);
+    let mut rules_matrix = vec![u8::MAX; unique_square];
     for ((before, after), between) in rules {
         rules_matrix[(before as usize * unique + after as usize)] = between;
     }
-    
-    let mut buffer = vec!(0u64; unique_square);
+
+    let mut buffer = vec![0u64; unique_square];
     for _ in 0..n {
         for (index, (&rule, &count)) in rules_matrix.iter().zip(actual.iter()).enumerate() {
             if rule == u8::MAX {
@@ -99,10 +116,13 @@ fn apply_n_rules_fast(value: Vec<ListItem>, rules: Vec<((u8, u8), u8)>, n: usize
         std::mem::swap(&mut actual, &mut buffer);
         buffer.clear();
         buffer.resize(unique_square, 0);
-    }    
+    }
     // Add last item to any field in it's row
     actual[last * unique] += 1;
-    actual.chunks(unique).map(|x| x.iter().sum::<u64>()).minmax()
+    actual
+        .chunks(unique)
+        .map(|x| x.iter().sum::<u64>())
+        .minmax()
 }
 
 fn get_min_max_occurance(items: Vec<ListItem>) -> MinMaxResult<u64> {
@@ -121,7 +141,7 @@ mod tests {
     #[test]
     fn part1_test() {
         let (mut init, rules, _) = parse(std::io::Cursor::new(TESTDATA)).unwrap();
-        let rules_map : HashMap<_, _> = rules.into_iter().collect();
+        let rules_map: HashMap<_, _> = rules.into_iter().collect();
         for _ in 0..10 {
             apply_rules(&mut init, &rules_map);
         }
@@ -134,8 +154,9 @@ mod tests {
 
     #[test]
     fn part1() {
-        let (mut init, rules, _) = parse(std::fs::File::open("puzzleData/day14.txt").unwrap()).unwrap();
-        let rules_map : HashMap<_, _> = rules.into_iter().collect();
+        let (mut init, rules, _) =
+            parse(std::fs::File::open("puzzleData/day14.txt").unwrap()).unwrap();
+        let rules_map: HashMap<_, _> = rules.into_iter().collect();
         for _ in 0..10 {
             apply_rules(&mut init, &rules_map);
         }
@@ -148,7 +169,8 @@ mod tests {
 
     #[test]
     fn part2() {
-        let (init, rules, unique) = parse(std::fs::File::open("puzzleData/day14.txt").unwrap()).unwrap();
+        let (init, rules, unique) =
+            parse(std::fs::File::open("puzzleData/day14.txt").unwrap()).unwrap();
         let result = apply_n_rules_fast(init, rules, 40, unique);
         if let MinMaxResult::MinMax(min, max) = result {
             assert_eq!(max - min, 2_158_894_777_814);
--- src/day15.rs
+++ src/day15.rs
@@ -1,28 +1,33 @@
 use itertools::Itertools;
-use pathfinding::{prelude::{Matrix, absdiff}, directed::astar::astar};
+use pathfinding::{
+    directed::astar::astar,
+    prelude::{absdiff, Matrix},
+};
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos(usize, usize);
 
 impl Pos {
-  fn distance(&self, other: &Pos) -> u32 {
-    (absdiff(self.0, other.0) + absdiff(self.1, other.1)) as u32
-  }
+    fn distance(&self, other: &Pos) -> u32 {
+        (absdiff(self.0, other.0) + absdiff(self.1, other.1)) as u32
+    }
 
-  fn successors(&self, m: &Matrix<u8>) -> Vec<(Pos, u32)> {
-      m.neighbours((self.0, self.1), false)
-        .map(|(x,y)| (Pos(x, y), m[(x, y)] as u32))
-        .collect()
-  }
+    fn successors(&self, m: &Matrix<u8>) -> Vec<(Pos, u32)> {
+        m.neighbours((self.0, self.1), false)
+            .map(|(x, y)| (Pos(x, y), m[(x, y)] as u32))
+            .collect()
+    }
 }
 
 fn calculate_shortest_path(m: &Matrix<u8>) -> u32 {
     let goal = Pos(m.columns - 1, m.rows - 1);
     dbg!(&goal);
-    let result = astar(&Pos(0, 0), 
-        |p| p.successors(&m), 
+    let result = astar(
+        &Pos(0, 0),
+        |p| p.successors(m),
         |p| p.distance(&goal),
-        |p| *p == goal);
+        |p| *p == goal,
+    );
     let (_, dist) = result.unwrap();
     dist
 }
@@ -31,37 +36,36 @@ const EXPAND: u8 = 5;
 fn wrap_value(i: u8, offset: u8) -> u8 {
     match i + offset {
         i if i < 10 => i,
-        i => i - 10 + 1
+        i => i - 10 + 1,
     }
 }
 fn read_part2_matrix(input: &str) -> Matrix<u8> {
-    
-    let mut vec = input.lines().map(|c| {
-        let mut vec = Vec::with_capacity(c.bytes().len() * EXPAND as usize);
-        for i in 0..EXPAND {
-            vec.extend(c.bytes().map(|x| {
-                let ret = wrap_value(x - b'0', i);
-                ret
-            }))
-        }
-        vec
-    }).collect_vec();
+    let mut vec = input
+        .lines()
+        .map(|c| {
+            let mut vec = Vec::with_capacity(c.bytes().len() * EXPAND as usize);
+            for i in 0..EXPAND {
+                vec.extend(c.bytes().map(|x| wrap_value(x - b'0', i)))
+            }
+            vec
+        })
+        .collect_vec();
     let height = vec.len();
     for i in 1..EXPAND {
         for inner in 0..height {
             vec.push(vec[inner].iter().map(|x| wrap_value(*x, i)).collect_vec());
         }
     }
-    
+
     vec.into_iter().collect()
 }
 
 fn print_matrix(m: &Matrix<u8>) {
     for y in 0..m.rows {
         for x in 0..m.columns {
-            print!("{}", m[(y,x)]);
+            print!("{}", m[(y, x)]);
         }
-        print!("\n");
+        println!();
     }
 }
 
@@ -78,7 +82,10 @@ mod tests {
     }
     #[test]
     fn part1_test() {
-        let m: Matrix<u8> = TESTDATA.lines().map(|c| c.bytes().map(|x| (x - b'0'))).collect();
+        let m: Matrix<u8> = TESTDATA
+            .lines()
+            .map(|c| c.bytes().map(|x| (x - b'0')))
+            .collect();
         assert_eq!(40, calculate_shortest_path(&m));
     }
 
